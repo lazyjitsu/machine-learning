5db2ee848706a0e38722d048f09e1674fa0ef437
Marko <graphneenja@gmail.com>
2024-12-04 13:31:49 -0800

    added button with styling

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -9,6 +9,11 @@ class SketchPad {
         `
         container.append(this.canvas);
 
+        this.lineBreak=document.createElement('br');
+        container.appendChild(this.lineBreak);
+        this.undoBtn=document.createElement('button');
+        this.undoBtn.innerHTML="Undo";
+        container.append(this.undoBtn);
         this.ctx=this.canvas.getContext("2d");
         this.paths=[];
         this.isDrawing=false;
@@ -16,13 +21,13 @@ class SketchPad {
 
     }
     #addEventListeners() {
-        this.onmousedown = (e) => {            
+        this.canvas.onmousedown = (e) => {            
             const mouse = this.#getMouse(e);
             // re-initalized after every mouse down event
             this.paths.push([mouse]);
             this.isDrawing=true;
         }
-        this.onmousemove = (e) => {
+        this.canvas.onmousemove = (e) => {
             if(this.isDrawing) {
                 const mouse = this.#getMouse(e);
                 const lastPath=this.paths[this.paths.length-1];
@@ -33,7 +38,7 @@ class SketchPad {
 
         }
 
-        this.onmouseup = () => {
+        this.canvas.onmouseup = () => {
             this.isDrawing=false;
         }
 
@@ -49,10 +54,19 @@ class SketchPad {
         this.canvas.ontouchend=() => {
             this.canvas.onmouseup();
         }
+        this.undoBtn.onclick=() => {
+            this.paths.pop();
+            this.#reDraw();
+        }
     }
     #reDraw=(e) => {
         this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
         draw.paths(this.ctx,this.paths);
+        if(this.paths.length>0) {
+            this.undoBtn.disabled=false;
+        } else {
+            this.undoBtn.disabled=true;
+        }
     }
     #getMouse=(e) => {
         const rect = this.canvas.getBoundingClientRect();
