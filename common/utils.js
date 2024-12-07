dd65dd51a653fa5b3bbdc1ef850a12aac9485932
Marko <graphneenja@gmail.com>
2024-12-07 11:53:27 -0800

    added some styling

--- common/utils.js
+++ common/utils.js
@@ -5,7 +5,16 @@ utils.flaggedUsers=[1663053145814]
 utils.formatPercent=(n) => {
     return (n*100).toFixed(2) + "%";
 }
-
+utils.styles={
+    car:'gray',
+    fish:'red',
+    house:'yellow',
+    tree:'green',
+    bicycle:'cyan',
+    guitar:'blue',
+    pencil:'magenta',
+    clock:'lightgray'
+}
 utils.printProgress=(count,max) => {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
