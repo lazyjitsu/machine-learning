519ed9bf2108a712007301cbfc10e324cdbcaa98
Marko <graphneenja@gmail.com>
2024-12-07 15:00:50 -0800

    fixed styles

--- common/utils.js
+++ common/utils.js
@@ -6,14 +6,14 @@ utils.formatPercent=(n) => {
     return (n*100).toFixed(2) + "%";
 }
 utils.styles={
-    car:{color:'gray',text:'🏫'}
-    fish:{color:'red',text:'🦈'}
-    house:{color:'yellow',text:'🏫'}
-    tree:{color:'green',text:'🌲'}
-    bicycle:{color:'cyan',text:'🚲'}
-    guitar:{color:'blue',text:'🎸'}
-    pencil:{color:'magenta',text:'✏️'}
-    clock:{color:'lightgray',text:'🕰️'}
+    car:{color:'gray',text:'🏫'},
+    fish:{color:'red',text:'🦈'},
+    house:{color:'yellow',text:'🏫'},
+    tree:{color:'green',text:'🌲'},
+    bicycle:{color:'cyan',text:'🚲'},
+    guitar:{color:'blue',text:'🎸'},
+    pencil:{color:'magenta',text:'✏️'},
+    clock:{color:'lightgray',text:'🕰️'},
 }
 utils.printProgress=(count,max) => {
     process.stdout.clearLine();
