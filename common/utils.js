ff07f77928f3d303152ca5de24cded395ed80a21
Marko <graphneenja@gmail.com>
2024-12-07 14:59:20 -0800

    added emojis to utils.styles

--- common/utils.js
+++ common/utils.js
@@ -6,14 +6,14 @@ utils.formatPercent=(n) => {
     return (n*100).toFixed(2) + "%";
 }
 utils.styles={
-    car:'gray',
-    fish:'red',
-    house:'yellow',
-    tree:'green',
-    bicycle:'cyan',
-    guitar:'blue',
-    pencil:'magenta',
-    clock:'lightgray'
+    car:{color:'gray',text:'🏫'}
+    fish:{color:'red',text:'🦈'}
+    house:{color:'yellow',text:'🏫'}
+    tree:{color:'green',text:'🌲'}
+    bicycle:{color:'cyan',text:'🚲'}
+    guitar:{color:'blue',text:'🎸'}
+    pencil:{color:'magenta',text:'✏️'}
+    clock:{color:'lightgray',text:'🕰️'}
 }
 utils.printProgress=(count,max) => {
     process.stdout.clearLine();
