ab2112191b2429a7f5b0d997ec9fd68e383ed1d3
Marko <graphneenja@gmail.com>
2024-12-08 08:52:06 -0800

    pixel and databound fcns

--- chart.js
+++ chart.js
@@ -11,6 +11,43 @@ class Chart {
         this.canvas.style="background-color:white;";
         container.appendChild(this.canvas);
 
+        this.ctx = this.canvas.getContext('2d');
+        this.margin = options.size*0.1; 
+        // because we have a lot of overlapping data points, we will use transparency
+        this.transparency=0.5;
 
+        this.pixelBounds=this.#getPixelBounds();
+        this.dataBounds=this.#getDataBounds();
+
+    
+    }
+
+    #getPixelBounds() {
+        const {canvas,margin} = this;
+        const bounds = {
+            left:margin,
+            right:canvas.width-margin,
+            top:margin,
+            bottom:canvas.height-margin
+        }
+        return bounds;
+    }
+    #getDataBounds() {
+        const {samples} = this;
+        const x = samples(s => s.point[0]);
+        const y = samples(s => s.point[1]);
+        // Math.min and max fcns don't work w/arrays, they work with individual values hence the spread operator ...
+
+        const minX = Math.min(...x);
+        const maxX = Math.max(...x);
+        const minY = Math.min(...y);
+        const maxY = Math.max(...y);
+        const bounds = {
+            left: minX,
+            right:maxX,
+            top:maxY,
+            bottom:minY
+        }
+        return bounds;
     }
 }
